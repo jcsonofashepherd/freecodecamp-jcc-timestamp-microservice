ceaebee3f66ec063211745450d54cab4a34309a9
System <none>
2017-10-03 07:18:08 +0000

    added timestamp functionality

--- server.js
+++ server.js
@@ -38,6 +38,66 @@ app.route('/')
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
 
+app.get("/\*", function (request, response) {
+  if (Number.isInteger(Number(request.url.substring(1)))) {
+    var date = new Date (Number(request.url.substring(1)) * 1000);
+  } else {
+    var date = new Date (request.url.substring(1).split("%20").join(""));
+  };
+  if (isNaN(date.getTime())) {
+    var packet = {
+      "unix": null,
+      "natural": null
+    }
+  } else {
+    var month = "";
+    switch (date.getMonth()) {
+      case 0:
+        month = "January";
+        break;
+      case 1:
+        month = "February";
+        break;
+      case 2:
+        month = "March";
+        break;
+      case 3:
+        month = "April";
+        break;
+      case 4:
+        month = "May";
+        break;
+      case 5:
+        month = "June";
+        break;
+      case 6:
+        month = "July";
+        break;
+      case 7:
+        month = "August";
+        break;
+      case 8:
+        month = "September";
+        break;
+      case 9:
+        month = "October";
+        break;
+      case 10:
+        month = "November";
+        break;
+      case 11:
+        month = "December";
+        break;
+    }
+    var packet = {
+        "unix": date.getTime() / 1000,
+        "natural": month + " " + date.getDate() + ", " + date.getFullYear()
+    };
+  }
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify(packet));
+});
+
 // Respond not found to all the wrong routes
 app.use(function(req, res, next){
   res.status(404);
